fe7eae29a284188080c7b0d265a63a61f47b39aa
abddusamad <63805036+abddusamad@users.noreply.github.com>
2020-05-16 16:42:54 +0530

    Create word_count.py

---
+++ word_count.py
@@ -0,0 +1,8 @@
+from collections import Counter #import Counter from collections module
+print("filename or path")
+path=str(input())               #taking input of name of path
+file=open(path)                 #opening path
+data=file.read()                #read file
+word=Counter(data.split())      #count words in the file and stored as dictionary values
+for i in word:
+    print(i,':',word[i])        #print words and count of their occurence
