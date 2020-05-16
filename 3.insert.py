004a888fbbdc38a0ad9bc2f2fc30716b9d313f78
abddusamad <63805036+abddusamad@users.noreply.github.com>
2020-05-16 21:54:12 +0530

    Update 3.insert.py

--- 3.insert.py
+++ 3.insert.py
@@ -1,8 +1,9 @@
 print("enter element,position and array")
+
 element=input()                             #input the element need to insert
 position=int(input())-1                     #position no.
 array=list(input().split())                 #input the array
-temp_array_first=array[:position]           #seperate the array at position number
-temp_array_last=array[position:]
-array=temp_array_first+[element]+temp_array_last    #adding all array using  
+arr1=array[:position]                       #seperate the array by position number
+arr2=array[position:]
+array=arr1+[element]+arr2                   #adding all array using concatenation 
 print(array)
